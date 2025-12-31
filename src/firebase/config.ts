ceecbd7252ecf48eaca999f37258658659939ced
Vivek Dhotre <164247102+VIVEKXDD@users.noreply.github.com>
2025-12-31 23:50:40 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3561434562-e8ee1",
-  "appId": "1:740942082069:web:d0b72aa17aece603385e1c",
-  "apiKey": "AIzaSyCLDfaPZC7w4GqFZ5nDsF1yRCkk--gsAGQ",
-  "authDomain": "studio-3561434562-e8ee1.firebaseapp.com",
+  "projectId": " ",
+  "appId": " ",
+  "apiKey": " ",
+  "authDomain": " ",
   "measurementId": "",
-  "messagingSenderId": "740942082069"
+  "messagingSenderId": " "
 };
